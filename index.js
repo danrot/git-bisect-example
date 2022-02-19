cf353e9b4685c7876f446e2e59bf05e5ea51cc31
Daniel Rotter <daniel.rotter@gmail.com>
2022-02-19 11:05:07 +0100

    Implement entering of numbers and addition

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const readline = require("readline-sync");
+
+const operand1 = parseInt(readline.question("First operator: "));
+const operand2 = parseInt(readline.question("Second operator: "));
+
+const result = operand1 + operand2;
+
+console.log(result);
