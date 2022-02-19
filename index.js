d3c66b49c330e58a70fe0abda56b691e1bb5db75
Daniel Rotter <daniel.rotter@gmail.com>
2022-02-19 11:07:48 +0100

    Refactor to use switch to differ between different operators

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const readline = require("readline-sync");
 
 const operand1 = parseInt(readline.question("First operator: "));
+const operator = readline.question("Operator: ");
 const operand2 = parseInt(readline.question("Second operator: "));
 
-const result = operand1 + operand2;
+let result;
+
+switch (operator) {
+	case "+":
+		result = operand1 + operand2;
+		break;
+}
 
 console.log(result);
